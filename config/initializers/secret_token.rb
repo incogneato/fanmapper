a9e4538532f40f4902191c76b23283183275812e
Apprentice <apprentice@devbootcamp.com>
2012-11-26 19:52:39 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanmapper::Application.config.secret_token = '46c262de2e471164afd214e34cd80aa5dbfcee10b8e6605cbee6792176f885951d0cc1c36c4110036b11b4d0a5cbaf073c82e6444fb18b6870c0dacb4b65ab80'
