3243c68dec8eb6811a78ef4cc2aa34d10b5e09ca
DBC Apprentice <apprentice@devbootcamp.com>
2012-12-03 12:31:41 -0800

    Add signup for bar owners to nav bar. Add bar_owner migration to User model. Add bar_owner hidden field that sets bar_owner true during signup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :bar_owner
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121203004808) do
+ActiveRecord::Schema.define(:version => 20121203202128) do
 
   create_table "bars", :force => true do |t|
     t.string   "name",                 :null => false
@@ -60,8 +60,8 @@ ActiveRecord::Schema.define(:version => 20121203004808) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -70,11 +70,12 @@ ActiveRecord::Schema.define(:version => 20121203004808) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "bar_owner",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
