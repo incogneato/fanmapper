8cf66dc6064c9423f81b345243df19e1a56bf3c4
Brian Lee <brian.g.lee89@gmail.com>
2012-12-04 23:14:16 -0800

    add form to create and update bars

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Fanmapper::Application.routes.draw do
                      controllers: { omniauth_callbacks: "omniauth_callbacks" }
 
   root to: 'home#index'
+
+   match "bars/:bar_id/add_game(.:format)" => "bars#add_game", :as => :add_game
+
   resources :bars
   resources :teams, only: [:show]
   resources :games, only: [:index, :show]
